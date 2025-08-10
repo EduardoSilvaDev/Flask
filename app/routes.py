c8e07d9885893e4c3a7f8975c08d7ef1f46587a8
EduardoSilvaDev <carlos.duhduh5569@gmail.com>
2025-08-10 10:28:07 -0300

    variaveis

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,12 @@ from flask import render_template, url_for
 @app.route('/')
 def home():
     usuario = 'Eduardo'
-    return render_template('index.html',usuario=usuario)
+    context = {
+        'user':'Eduardo',
+        'idade':30,
+        'city':'São Caitano'
+    }
+    return render_template('index.html',usuario=usuario,context=context)
 
 
 @app.route('/novo')
