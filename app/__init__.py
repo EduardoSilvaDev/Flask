7c52964daee0a0eb93f848df79164a820719651d
EduardoSilvaDev <carlos.duhduh5569@gmail.com>
2025-08-14 21:31:05 -0300

    criacao de variavel de ambiente e tornando mais seguro com secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # não é obrigatorio
-app.config['SECRET_KEY'] = 'ASDAS5JDK2LNASD_ASNDAASSDNÇ213123LKAJSDLKJ!AS512DJALSKDNASD'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app=app)
 migrate = Migrate(app=app,db=db)
